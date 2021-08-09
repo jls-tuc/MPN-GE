85bc5e0cf09e62f71fc329c76f70a0eae2c7b607
Jose Luis <65769995+jls-tuc@users.noreply.github.com>
2021-08-09 20:05:59 -0300

    cambios-graficas-menu-datosUs-css

--- src/app/shared/components/sidebar-top/sidebar-top.component.ts
+++ src/app/shared/components/sidebar-top/sidebar-top.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from "@angular/core";
 // import PerfectScrollbar from 'perfect-scrollbar';
 import { NavigationService } from "../../../shared/services/navigation.service";
 import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-sidebar-top',
-  templateUrl: './sidebar-top.component.html'
+  selector: "app-sidebar-top",
+  templateUrl: "./sidebar-top.component.html",
 })
 export class SidebarTopComponent implements OnInit, OnDestroy, AfterViewInit {
   // private sidebarPS: PerfectScrollbar;
   public menuItems: any[];
   private menuItemsSub: Subscription;
-  constructor(
-    private navService: NavigationService
-  ) { }
+  constructor(private navService: NavigationService) {}
 
   ngOnInit() {
-    this.menuItemsSub = this.navService.menuItems$.subscribe(menuItem => {
-      this.menuItems = menuItem.filter(item => item.type !== 'icon' && item.type !== 'separator');
+    this.menuItemsSub = this.navService.menuUse$.subscribe((menuItem) => {
+      this.menuItems = menuItem.menu.filter(
+        (item) => item.type !== "icon" && item.type !== "separator"
+      );
     });
   }
   ngAfterViewInit() {
@@ -31,9 +31,8 @@ export class SidebarTopComponent implements OnInit, OnDestroy, AfterViewInit {
     // if(this.sidebarPS) {
     //   this.sidebarPS.destroy();
     // }
-    if( this.menuItemsSub ) {
-      this.menuItemsSub.unsubscribe()
+    if (this.menuItemsSub) {
+      this.menuItemsSub.unsubscribe();
     }
   }
-
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { config } from "config";
 export const environment = {
   production: true,
   //apiURL: "http://181.209.23.177:3000/api",
-  apiURL: "https://181.209.23.177:8017/api",
+  apiURL: "https://paso2021nqn.com.ar:3000/api",
 };
